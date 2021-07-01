0bade878120c3752b07284f743b6ba24d334185c
RSugimoto2017 <sryosuke2017@gmail.com>
2021-07-02 02:28:06 +0900

    Add function bun Error

---
+++ src/Swarm.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Particle.h"
+class Particle;
+
+class Swarm
+{
+public:
+  Swarm(char *filename);
+  ~Swarm();
+  void move();        //粒子を移動する
+  void printResult(); //結果を表示する
+
+  Dataset *dataset;    //データセット
+  Particle **particle; //粒子群のメンバ
+  double *gBestPos;    //グローバルベストの位置
+  double gBestValue;   //グローバルベストの評価値
+};
